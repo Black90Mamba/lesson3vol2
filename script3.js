bc78b527a01a509d4f9658a75b4e1d3367c863e8
Petra Volkova <volkovapetra17@gmail.com>
2024-10-07 19:13:17 +0200

    dalsi

--- script3.js
+++ script3.js
@@ -1,9 +1,5 @@
-console.log("hello world")
-const hodinovka = 300
-const pocetHodin = Number(prompt("zadej pocet hodin zamestnance", "160")) //number je nutné použít abych věděla že se jedná o čísla ne o text
-const premie = Number(prompt("zadej mimoradnou odmenu v Kc"))
-const plat = (hodinovka * pocetHodin) + premie
 
-document.body.innerHTML = "<p>Pepa Novak si vydelal: " + plat + " Kc.</p>"
+const jmeno = prompt("zadejte jmeno")
+const vek = Number(prompt("Zadejte vek"))
 
-
+document.body.innerHTML = "<p>Údaje: " + jmeno + ", věk: " + vek + "</p>";
